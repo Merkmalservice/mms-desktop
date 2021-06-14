2a7591e42cc08cc4155a5f02d83074f3487a44a1
quasarchimaere <sudafabian@gmail.com>
2021-06-14 13:04:16 +0200

    Style and add convert button

--- src/main/java/at/researchstudio/sat/mmsdesktop/controller/ExtractController.java
+++ src/main/java/at/researchstudio/sat/mmsdesktop/controller/ExtractController.java
@@ -8,72 +8,91 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.invoke.MethodHandles;
 import java.net.URL;
 import java.nio.file.NotDirectoryException;
-import java.util.HashSet;
-import java.util.List;
-import java.util.ResourceBundle;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Component
 public class ExtractController implements Initializable {
+  private static final Logger logger =
+          LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-    private FileChooser fileChooser;
-    private DirectoryChooser directoryChooser;
+  @FXML private JFXButton clearList;
+  @FXML private JFXButton convertButton;
+  @FXML private JFXButton removeSelectedEntry;
+  @FXML private BorderPane borderPane;
+  @FXML private JFXButton pickFile;
+  @FXML private JFXButton pickDirectory;
+  @FXML private TableView ifcFileTable;
 
-    @FXML private AnchorPane anchorPane;
+  private FileChooser fileChooser;
+  private DirectoryChooser directoryChooser;
+  private Set<FileWrapper> selectedIfcFiles;
 
-    @FXML private JFXButton pickFile;
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+    fileChooser = new FileChooser();
+    fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("IFC Files", "*.ifc"));
 
-    @FXML private JFXButton pickDirectory;
+    directoryChooser = new DirectoryChooser();
+    selectedIfcFiles = new HashSet<>();
+    ifcFileTable.setItems(FXCollections.observableArrayList(selectedIfcFiles));
+  }
 
-    @FXML private TableView ifcFileTable;
-
-    private Set<FileWrapper> selectedIfcFiles;
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        fileChooser = new FileChooser();
-        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("IFC Files", "*.ifc"));
-
-        directoryChooser = new DirectoryChooser();
-        selectedIfcFiles = new HashSet<>();
+  @FXML
+  public void handlePickDirectoryAction(ActionEvent actionEvent) {
+    File selectedDirectory = directoryChooser.showDialog(borderPane.getScene().getWindow());
+    try {
+      List<File> selectedFiles = FileUtils.getIfcFilesFromDirectory(selectedDirectory);
+      if (selectedFiles.size() > 0) {
+        selectedIfcFiles.addAll(
+            selectedFiles.stream().map(FileWrapper::new).collect(Collectors.toList()));
         ifcFileTable.setItems(FXCollections.observableArrayList(selectedIfcFiles));
+        clearList.setDisable(false);
+        convertButton.setDisable(false);
+      }
+    } catch (FileNotFoundException | NotDirectoryException e) {
+      logger.warn("No Valid Directory selected");
     }
+  }
 
-    @FXML
-    public void handlePickDirectoryAction(ActionEvent actionEvent) {
-        File selectedDirectory = directoryChooser.showDialog(anchorPane.getScene().getWindow());
-        try {
-            selectedIfcFiles.addAll(FileUtils.getIfcFilesFromDirectory(selectedDirectory).stream().map(f -> new FileWrapper(f)).collect(Collectors.toList()));
-            ifcFileTable.setItems(FXCollections.observableArrayList(selectedIfcFiles));
-        } catch (FileNotFoundException e) {
-            //TODO: HANDLE ERROR
-        } catch (NotDirectoryException e) {
-            //TODO: HANDLE ERROR
-        }
+  @FXML
+  public void handlePickFileAction(ActionEvent actionEvent) {
+    List<File> selectedFiles =
+        fileChooser.showOpenMultipleDialog(borderPane.getScene().getWindow());
+    if (Objects.nonNull(selectedFiles) && selectedFiles.size() > 0) {
+      selectedIfcFiles.addAll(
+          selectedFiles.stream().map(FileWrapper::new).collect(Collectors.toList()));
+      ifcFileTable.setItems(FXCollections.observableArrayList(selectedIfcFiles));
+      clearList.setDisable(false);
+      convertButton.setDisable(false);
     }
+  }
 
-    @FXML
-    public void handlePickFileAction(ActionEvent actionEvent) {
-        List<File> selectedFiles = fileChooser.showOpenMultipleDialog(anchorPane.getScene().getWindow());
+  @FXML
+  public void handleClearListAction(ActionEvent actionEvent) {
+    selectedIfcFiles.clear();
+    ifcFileTable.setItems(FXCollections.observableArrayList(selectedIfcFiles));
+    clearList.setDisable(true);
+    convertButton.setDisable(true);
+  }
 
-        selectedIfcFiles.addAll(selectedFiles.stream().map(f -> new FileWrapper(f)).collect(Collectors.toList()));
-        ifcFileTable.setItems(FXCollections.observableArrayList(selectedIfcFiles));
-    }
+  @FXML
+  public void handleRemoveSelectedEntryAction(ActionEvent actionEvent) {
+  }
 
-    @FXML
-    public void handleClearListAction(ActionEvent actionEvent) {
-        selectedIfcFiles.clear();
-        ifcFileTable.setItems(FXCollections.observableArrayList(selectedIfcFiles));
-    }
+  @FXML
+  public void handleConvertAction(ActionEvent actionEvent) {
+  }
 }
