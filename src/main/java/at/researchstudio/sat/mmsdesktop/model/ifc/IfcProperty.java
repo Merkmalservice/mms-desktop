a82593d0601ea5c390d66c2d82e54be67604f17b
quasarchimaere <sudafabian@gmail.com>
2021-06-24 14:13:12 +0200

    Removes duplicate code and simplify constructors

--- src/main/java/at/researchstudio/sat/mmsdesktop/model/ifc/IfcProperty.java
+++ src/main/java/at/researchstudio/sat/mmsdesktop/model/ifc/IfcProperty.java
@@ -5,7 +5,6 @@ import at.researchstudio.sat.mmsdesktop.model.ifc.vocab.IfcUnitMeasure;
 import at.researchstudio.sat.mmsdesktop.model.ifc.vocab.IfcUnitType;
 import at.researchstudio.sat.mmsdesktop.util.Utils;
 import org.apache.jena.query.QuerySolution;
-import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,38 +15,34 @@ import java.util.Map;
 import java.util.Objects;
 
 public class IfcProperty {
-    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+  private static final Logger logger =
+      LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-    private final String name;
-    private final IfcPropertyType type;
+  private final String name;
+  private final IfcPropertyType type;
 
-    private IfcUnitMeasure measure;
+  private IfcUnitMeasure measure;
 
-    public IfcProperty(IfcProperty ifc, Map<IfcUnitType, List<IfcUnit>> projectUnits) {
-    this(ifc.name, ifc.type);
+  public IfcProperty(IfcProperty ifc, Map<IfcUnitType, List<IfcUnit>> projectUnits) {
+    this.name = Utils.convertIFCStringToUtf8(ifc.name);
+    this.type = ifc.type;
 
     if (this.type.isMeasureType()) {
-      IfcUnitMeasure tempMeasure = IfcUnitMeasure.UNKNOWN;
-      if (Objects.nonNull(projectUnits)) {
-        IfcUnitType tempUnitType = this.type.getUnitType();
-        List<IfcUnit> units = projectUnits.get(tempUnitType);
-
-        if (Objects.nonNull(units)) {
-          if (units.size() == 1) {
-            IfcUnit ifcUnit = units.get(0);
-            tempMeasure = ifcUnit.getMeasure();
-          } else {
-            logger.debug("More than one unit present, leaving it empty");
-            units.forEach(unit -> logger.debug(unit.toString()));
-          }
-        }
-      }
-      this.measure = tempMeasure;
+      this.measure = generateMeasureFromProjectUnits(ifc.type, projectUnits);
     }
   }
 
-    public IfcProperty(QuerySolution qs, Map<IfcUnitType, List<IfcUnit>> projectUnits) {
-    this(qs.getLiteral("propName"), qs.getResource("propType"));
+  public IfcProperty(QuerySolution qs, Map<IfcUnitType, List<IfcUnit>> projectUnits) {
+    this.name = Utils.convertIFCStringToUtf8(qs.getLiteral("propName").toString());
+
+    IfcPropertyType tempType = IfcPropertyType.UNKNOWN;
+    try {
+      tempType = IfcPropertyType.fromResource(qs.getResource("propType"));
+    } catch (IllegalArgumentException e) {
+      logger.error(e.getMessage());
+    }
+
+    this.type = tempType;
 
     if (this.type.isMeasureType()) {
       // TODO: update query and add optional propMeasure to add the ifc unit to the query output
@@ -62,85 +57,72 @@ public class IfcProperty {
         } catch (IllegalArgumentException e) {
           logger.error(e.getMessage());
         }
-        this.measure = tempMeasure;
-      } else if (Objects.nonNull(projectUnits)) {
-        IfcUnitType tempUnitType = this.type.getUnitType();
-        List<IfcUnit> units = projectUnits.get(tempUnitType);
-
-        if (Objects.nonNull(units)) {
-          if (units.size() == 1) {
-            IfcUnit ifcUnit = units.get(0);
-            this.measure = ifcUnit.getMeasure();
-          } else {
-            logger.debug("More than one unit present, leaving it empty");
-            units.forEach(unit -> logger.debug(unit.toString()));
-          }
-        }
+      } else {
+        tempMeasure = generateMeasureFromProjectUnits(this.type, projectUnits);
       }
+      this.measure = tempMeasure;
     }
   }
 
-    // TODO: ADD PROJECTUNITS TO CONSTRUCTOR
-    public IfcProperty(String name, String type) {
-        this.name = Utils.convertIFCStringToUtf8(name);
+  public IfcProperty(String name, String type) {
+    this.name = Utils.convertIFCStringToUtf8(name);
 
-        IfcPropertyType tempType = IfcPropertyType.UNKNOWN;
-        try {
-            tempType = IfcPropertyType.fromString(type);
-        } catch (IllegalArgumentException e) {
-            logger.error(e.getMessage());
+    IfcPropertyType tempType = IfcPropertyType.UNKNOWN;
+    try {
+      tempType = IfcPropertyType.fromString(type);
+    } catch (IllegalArgumentException e) {
+      logger.error(e.getMessage());
+    }
+
+    this.type = tempType;
+  }
+
+  private static IfcUnitMeasure generateMeasureFromProjectUnits(IfcPropertyType type,
+      Map<IfcUnitType, List<IfcUnit>> projectUnits) {
+    if (Objects.nonNull(projectUnits)) {
+      IfcUnitType tempUnitType = type.getUnitType();
+      List<IfcUnit> units = projectUnits.get(tempUnitType);
+
+      if (Objects.nonNull(units)) {
+        if (units.size() == 1) {
+          IfcUnit ifcUnit = units.get(0);
+          return ifcUnit.getMeasure();
+        } else {
+          logger.debug("More than one unit present, leaving it empty");
+          units.forEach(unit -> logger.debug(unit.toString()));
         }
-
-        this.type = tempType;
+      }
     }
+    return IfcUnitMeasure.UNKNOWN;
+  }
 
-    public IfcProperty(String name, IfcPropertyType type) {
-        this.name = Utils.convertIFCStringToUtf8(name);
-        this.type = type;
-    }
+  public String getName() {
+    return name;
+  }
 
-    private IfcProperty(Literal name, Resource type) {
-        this.name = Utils.convertIFCStringToUtf8(name.toString());
+  public IfcPropertyType getType() {
+    return type;
+  }
 
-        IfcPropertyType tempType = IfcPropertyType.UNKNOWN;
-        try {
-            tempType = IfcPropertyType.fromResource(type);
-        } catch (IllegalArgumentException e) {
-            logger.error(e.getMessage());
-        }
+  public IfcUnitMeasure getMeasure() {
+    return measure;
+  }
 
-        this.type = tempType;
-    }
+  @Override public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null || getClass() != o.getClass())
+      return false;
+    IfcProperty that = (IfcProperty) o;
+    return Objects.equals(name, that.name) && type == that.type && measure == that.measure;
+  }
 
-    public String getName() {
-        return name;
-    }
+  @Override public int hashCode() {
+    return Objects.hash(name, type, measure);
+  }
 
-    public IfcPropertyType getType() {
-        return type;
-    }
-
-    public IfcUnitMeasure getMeasure() {
-        return measure;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-        IfcProperty that = (IfcProperty) o;
-        return Objects.equals(name, that.name) && type == that.type && measure == that.measure;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, type, measure);
-    }
-
-    @Override
-    public String toString() {
-        return "IfcProperty{" + "name='" + name + '\'' + ", type=" + type + ", measure=" + measure + '}';
-    }
+  @Override public String toString() {
+    return "IfcProperty{" + "name='" + name + '\'' + ", type=" + type + ", measure=" + measure
+        + '}';
+  }
 }
