536c9719a95af0e60acd77d5536380588ac56a9c
quasarchimaere <sudafabian@gmail.com>
2021-08-12 14:33:21 +0200

    Adding numeric type unit/kind and enum option values for selected feature

--- src/main/java/at/researchstudio/sat/mmsdesktop/controller/components/FeatureComponentController.java
+++ src/main/java/at/researchstudio/sat/mmsdesktop/controller/components/FeatureComponentController.java
@@ -1,13 +1,17 @@
 package at.researchstudio.sat.mmsdesktop.controller.components;
 
+import at.researchstudio.sat.merkmalservice.model.EnumFeature;
 import at.researchstudio.sat.mmsdesktop.service.ReactiveStateService;
 import com.jfoenix.controls.JFXTextArea;
 import com.sandec.mdfx.MarkdownView;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
@@ -15,11 +19,16 @@ import org.springframework.stereotype.Component;
 @Component
 @FxmlView("featureComponent.fxml")
 public class FeatureComponentController implements Initializable {
+    @FXML public GridPane featureNumericType;
+    @FXML public TableView featureEnumType;
     @FXML private VBox parentPane;
     @FXML private Label featureName;
     @FXML private Label featureType;
     @FXML private MarkdownView featureDescriptionMarkdown;
     @FXML private JFXTextArea featureJson;
+
+    @FXML private Label featureUnit;
+    @FXML private Label featureQuantityKind;
 
     private final ReactiveStateService stateService;
 
@@ -41,5 +50,30 @@ public class FeatureComponentController implements Initializable {
         this.featureJson
                 .textProperty()
                 .bind(stateService.getSelectedFeatureState().featureJsonProperty());
+
+        this.featureQuantityKind
+                .textProperty()
+                .bind(stateService.getSelectedFeatureState().featureQuantityKindProperty());
+        this.featureUnit
+                .textProperty()
+                .bind(stateService.getSelectedFeatureState().featureUnitProperty());
+
+        this.featureNumericType
+                .managedProperty()
+                .bind(stateService.getSelectedFeatureState().featureIsNumericProperty());
+        this.featureNumericType
+                .visibleProperty()
+                .bind(stateService.getSelectedFeatureState().featureIsNumericProperty());
+
+        this.featureEnumType
+                .managedProperty()
+                .bind(stateService.getSelectedFeatureState().featureIsEnumerationProperty());
+        this.featureEnumType
+                .visibleProperty()
+                .bind(stateService.getSelectedFeatureState().featureIsEnumerationProperty());
+    }
+
+    public ObservableList<EnumFeature.OptionValue> getFeatureOptionValues() {
+        return stateService.getSelectedFeatureState().getFeatureOptionValues();
     }
 }
--- src/main/java/at/researchstudio/sat/mmsdesktop/state/SelectedFeatureState.java
+++ src/main/java/at/researchstudio/sat/mmsdesktop/state/SelectedFeatureState.java
@@ -1,30 +1,48 @@
 package at.researchstudio.sat.mmsdesktop.state;
 
 import at.researchstudio.sat.merkmalservice.model.*;
+import at.researchstudio.sat.mmsdesktop.util.MessageUtils;
 import at.researchstudio.sat.mmsdesktop.util.Utils;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import java.util.Locale;
 import java.util.Objects;
+import java.util.ResourceBundle;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SelectedFeatureState {
+    private final ResourceBundle resourceBundle;
+
     private final BooleanProperty showSelectedFeature;
+    private final BooleanProperty featureIsNumeric;
+    private final BooleanProperty featureIsEnumeration;
     private final SimpleStringProperty featureName;
     private final SimpleStringProperty featureType;
     private final SimpleStringProperty featureDescription;
-
+    private final SimpleStringProperty featureUnit;
+    private final SimpleStringProperty featureQuantityKind;
     private final SimpleStringProperty featureJson;
 
+    private final ObservableList<EnumFeature.OptionValue> featureOptionValues;
+
     public SelectedFeatureState() {
+        this.resourceBundle = ResourceBundle.getBundle("messages", Locale.getDefault());
         this.showSelectedFeature = new SimpleBooleanProperty(false);
+        this.featureIsNumeric = new SimpleBooleanProperty(false);
+        this.featureIsEnumeration = new SimpleBooleanProperty(false);
         this.featureName = new SimpleStringProperty("");
         this.featureType = new SimpleStringProperty("");
         this.featureDescription = new SimpleStringProperty("");
         this.featureJson = new SimpleStringProperty("");
+        this.featureUnit = new SimpleStringProperty("");
+        this.featureQuantityKind = new SimpleStringProperty("");
+        this.featureOptionValues = FXCollections.observableArrayList();
     }
 
     public BooleanProperty showSelectedFeatureProperty() {
@@ -43,15 +61,30 @@ public class SelectedFeatureState {
         this.featureType.setValue(
                 Utils.executeOrDefaultOnException(
                         () -> {
+                            this.featureIsNumeric.setValue(false);
+                            this.featureIsEnumeration.setValue(false);
+                            this.featureOptionValues.clear();
                             if (feature instanceof StringFeature) {
                                 return "TEXT";
                             } else if (feature instanceof EnumFeature) {
+                                this.featureIsEnumeration.setValue(true);
+                                this.featureOptionValues.setAll(
+                                        ((EnumFeature) feature).getOptions());
                                 return "ENUM";
                             } else if (feature instanceof ReferenceFeature) {
                                 return "REFERENCE";
                             } else if (feature instanceof BooleanFeature) {
                                 return "BOOLE";
                             } else if (feature instanceof NumericFeature) {
+                                this.featureIsNumeric.setValue(true);
+                                this.featureQuantityKind.setValue(
+                                        MessageUtils.getKeyForQuantityKind(
+                                                resourceBundle,
+                                                ((NumericFeature) feature).getQuantityKind()));
+                                this.featureUnit.setValue(
+                                        MessageUtils.getKeyForUnit(
+                                                resourceBundle,
+                                                ((NumericFeature) feature).getUnit()));
                                 return "NUMERIC";
                             } else {
                                 return "<no valid type>";
@@ -77,6 +110,26 @@ public class SelectedFeatureState {
 
     public SimpleStringProperty featureDescriptionProperty() {
         return featureDescription;
+    }
+
+    public SimpleStringProperty featureUnitProperty() {
+        return featureUnit;
+    }
+
+    public SimpleStringProperty featureQuantityKindProperty() {
+        return featureQuantityKind;
+    }
+
+    public BooleanProperty featureIsNumericProperty() {
+        return featureIsNumeric;
+    }
+
+    public BooleanProperty featureIsEnumerationProperty() {
+        return featureIsEnumeration;
+    }
+
+    public ObservableList<EnumFeature.OptionValue> getFeatureOptionValues() {
+        return featureOptionValues;
     }
 
     public SimpleStringProperty featureJsonProperty() {
