8d0c5f060311211747c4b5273bc2f841164e76d2
maxstolze <maximilian.stolze@researchstudio.at>
2021-09-14 12:18:17 +0200

    Format AboutController

--- src/main/java/at/researchstudio/sat/mmsdesktop/controller/AboutController.java
+++ src/main/java/at/researchstudio/sat/mmsdesktop/controller/AboutController.java
@@ -1,14 +1,13 @@
 package at.researchstudio.sat.mmsdesktop.controller;
 
 import com.sandec.mdfx.MarkdownView;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.BorderPane;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
-
-import java.net.URL;
-import java.util.ResourceBundle;
 
 @Component
 @FxmlView("about.fxml")
@@ -22,33 +21,33 @@ public class AboutController implements Initializable {
         // reference
         // centerAboutMarkdown = new MarkdownView("#bla ##blabla");
         centerAboutMarkdown.setMdString(
-                        "## Merkmalservice Desktop App\n"
-                                        + "\n"
-                                        + "### Version\n"
-                                        + "- 0.0.3 Beta\n"
-                                        + "### Usage\n"
-                                        + "##### Extract Features from IFC (or previously extracted JSON):\n"
-                                        + "- Add to List:\n"
-                                        + "    - Select single Files\n"
-                                        + "    - Select all Files in a Folder\n"
-                                        + "- Optional: Remove all Files again\n"
-                                        + "- Extract/Open:\n"
-                                        + "    - All Features will be extracted from Files\n"
-                                        + "- View Extracted Features from IFC:\n"
-                                        + "     - View Features in Table\n"
-                                        + "     - View Specific Features and all unique extracted Values\n"
-                                        + "- Preview JSON (Features JSON)"
-                                        + "- Export:\n"
-                                        + "    - JSON (with or without Unique Values in Description)\n"
-                                        + "    - Logfile\n"
-                                        + "- Check Log Output\n"
-                                        + "##### Convert IFC\n"
-                                        + "- Select IFC File\n"
-                                        + "- Close File again\n"
-                                        + "- View raw IFC lines (File)\n"
-                                        + "- Inspekt Feautres (Inspect):\n"
-                                        + "     - Show Features in IFC File (line based)\n"
-                                        + "### Support\n"
-                                        + " > sat@researchstudio.at");
+                "## Merkmalservice Desktop App\n"
+                        + "\n"
+                        + "### Version\n"
+                        + "- 0.0.3 Beta\n"
+                        + "### Usage\n"
+                        + "##### Extract Features from IFC (or previously extracted JSON):\n"
+                        + "- Add to List:\n"
+                        + "    - Select single Files\n"
+                        + "    - Select all Files in a Folder\n"
+                        + "- Optional: Remove all Files again\n"
+                        + "- Extract/Open:\n"
+                        + "    - All Features will be extracted from Files\n"
+                        + "- View Extracted Features from IFC:\n"
+                        + "     - View Features in Table\n"
+                        + "     - View Specific Features and all unique extracted Values\n"
+                        + "- Preview JSON (Features JSON)"
+                        + "- Export:\n"
+                        + "    - JSON (with or without Unique Values in Description)\n"
+                        + "    - Logfile\n"
+                        + "- Check Log Output\n"
+                        + "##### Convert IFC\n"
+                        + "- Select IFC File\n"
+                        + "- Close File again\n"
+                        + "- View raw IFC lines (File)\n"
+                        + "- Inspekt Feautres (Inspect):\n"
+                        + "     - Show Features in IFC File (line based)\n"
+                        + "### Support\n"
+                        + " > sat@researchstudio.at");
     }
 }
